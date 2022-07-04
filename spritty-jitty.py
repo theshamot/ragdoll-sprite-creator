2955823e5e16016588cda7a365e905cecd7a8deb
Tomas Rompotl <tomasrompotl@cermitech.com>
2022-07-04 10:42:42 +0200

    basic layout

--- spritty-jitty.py
+++ spritty-jitty.py
@@ -3,18 +3,19 @@ import PySimpleGUI as sg
 menu_def = [['&File', ['!&Open', '&Save::savekey', '&Save as::saveaskey', '---', '&Settings', 'E&xit']],
             ['&Edit', ['!&Paste', 'Undo'], ],
             ['&Help', '&About'], ]
-            
+
+# left side layout            
 left_toolbar = [
-    [sg.Text("Toolbox")],
-    [sg.Button("1")],
-    [sg.Button("2")],
-    [sg.Button("3")],
+    sg.Text("Toolbox"),
+    sg.Button("1"),
+    sg.Button("2"),
+    sg.Button("3"),
 ]
 left_workspace = [
-    [sg.Canvas(size=(500,500), background_color="#fff")]
+    sg.Canvas(size=(500,500), background_color="#fff")
 ]
 left_browser = [
-    []
+    sg.Listbox(values=[], size=(50, 5))
 ]
 left_layout = [
     left_toolbar,
@@ -22,15 +23,34 @@ left_layout = [
     left_browser
 ]
 
+# right side layout
+right_toolbar = [
+    sg.Text("Toolbox"),
+    sg.Button("1"),
+    sg.Button("2"),
+    sg.Button("3"),
+]
+right_workspace = [
+    sg.Canvas(size=(500,500), background_color="#fff")
+]
+right_browser = [
+    sg.Listbox(values=[], size=(50, 5))
+]
+right_layout = [
+    right_toolbar,
+    right_workspace,
+    right_browser
+]
+
+# create main window
 layout = [
     [
         sg.Menu(menu_def, tearoff=False),
         sg.Column(left_layout),
         sg.VSeparator(),
-        sg.Column(left_layout),
+        sg.Column(right_layout),
     ]
 ]
-
 window = sg.Window(title="Ragdoll sprite creator", layout=layout)
 
 while True:
